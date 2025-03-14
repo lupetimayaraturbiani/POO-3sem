f411de70871a154070771da4fd312259671ab181
MAYARA LUPETI TURBIANI <mayara.lturbiani@senacsp.edu.br>
2025-03-13 21:53:08 -0300

    comentários para modificações do trabalho que ele vai passar

--- Jogo/src/App.java
+++ Jogo/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //criar novos inimigos
+        //um apenas atributos, um modifica o ataque e outro modifica e outro modifica o tomaDano
+        //criar um metodo que recebe duas criaturas e faz a batalha
+
+
         Jogador jogador = new Jogador("mayara");
         //Criatura inimigo = new Malignus();
         Criatura inimigo = new Jogador("joana");
